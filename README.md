b7520402ac6901c922a9c6c3709c038a7884a104
rick2600 <ricks2600@gmail.com>
2016-12-09 23:29:11 -0300

    plugin code

---
+++ README.md
@@ -0,0 +1,28 @@
+# List Comments (v1.0 alpha)
+Author: **rick2600**
+
+_List all comments._
+
+## Description:
+
+List all comments the user wrote.
+
+## Minimum Version
+
+This plugin requires the following minimum version of Binary Ninja:
+
+ * release - 9999
+ * dev - 1.0.dev-576
+
+
+## Required Dependencies
+
+The following dependencies are required for this plugin:
+
+
+
+## License
+
+This plugin is released under a [MIT](LICENSE) license.
+
+
---
+++ __init__.py
@@ -0,0 +1,16 @@
+from binaryninja import *
+
+
+def do_formatting(comment):
+  return comment.replace("\n", "\\n")
+
+def list_comments(bv):
+    all_comments = []
+    for func in bv.functions:
+        for address, comment in func.comments.iteritems():
+            content = "0x%x    %s" %(int(address), do_formatting(comment))
+            all_comments.append(content)
+           
+    show_plain_text_report("List Comments", "\n".join(all_comments))
+
+PluginCommand.register("List Comments", "List all comments", list_comments)
